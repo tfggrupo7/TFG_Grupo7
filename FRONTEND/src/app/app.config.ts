f920f0d7373aa5af4ebff2330089d6d5c7de732b
fanciscogonzalezp <franciscogonzalezparra0@gmail.com>
2025-05-27 09:32:08 +0200

    Creado un interceptor sencillo y algunas carpetas de la estructura

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    )
   ],
 };
