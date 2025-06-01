885c14f6aa0afb814384569e31e104a9184afb2c
tfggrupo7 <tfggrupo7@gmail.com>
2025-06-01 15:00:53 +0200

    Delete BACKEND directory

--- BACKEND/apiTasksRest-backEnd/src/routes/api/menusPlatos.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const { getAll, create, remove} = require('../../controllers/menusPlatos.controller')
-
-router.get("/", getAll);
-router.post("/", create);
-router.delete("/menus/:menuId", remove)
-
-module.exports = router;
