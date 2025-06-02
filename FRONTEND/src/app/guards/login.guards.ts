2105afdc4799bd7726f8ccfa248018b6c28b7af7
Davdonoso <waltra2@gmail.com>
2025-06-02 08:32:34 +0200

    Reparacion de bugs

---
+++ FRONTEND/src/app/guards/login.guards.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const loginGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  //redux, stagejs son librerias que trabajan de manera similar al localstorage
+  let token = localStorage.getItem('token') || null
+  if (!token) {
+    router.navigate(['/login'])
+    return false;
+  }
+  return true;
+};
