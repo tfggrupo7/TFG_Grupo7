6109a134cf468b5df263c5877b11d84ba09ccb53
fanciscogonzalezp <franciscogonzalezparra0@gmail.com>
2025-06-04 12:50:42 +0200

    Cambio forma de hacer las modales por un sistema mas sencillo

--- FRONTEND/src/app/shared/components/modal/modal.component.ts
+++ FRONTEND/src/app/shared/components/modal/modal.component.ts
@@ -1,29 +1,19 @@
-import { Component, Type, ViewChild, ViewContainerRef, OnDestroy, ChangeDetectionStrategy, EnvironmentInjector, Input} from '@angular/core';
+import { Component, Input, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: []
 })
-export class ModalComponent implements OnDestroy {
+export class ModalComponent {
 
-  @ViewChild('container', { read: ViewContainerRef, static: true }) container!: ViewContainerRef;
+  @Output() closeEvent = new EventEmitter<void>();
 
-  public closeFn: () => void = () => {};
-
-  loadComponent<T>(component: Type<T>, injector: EnvironmentInjector) {
-    this.container.clear();
-    this.container.createComponent(component, { environmentInjector: injector });
-  }
+  @Input() btnSendName: string = 'Enviar'
 
   close() {
-    this.closeFn();
-  }
-
-  ngOnDestroy() {
-    this.container.clear();
+    this.closeEvent.emit();
   }
 }
