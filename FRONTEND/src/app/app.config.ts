d273ad0c5393d750426bc9e2f7ec413a94a4e1f5
RodrigoSendinoSanz <rodrigosendinosanz@gmail.com>
2025-06-10 22:32:59 +0200

    SCRUM-48-creacion-pwa-y-estilos-del-scroll

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -2,12 +2,23 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig = {
   providers: [
     provideRouter(routes,withComponentInputBinding()),
     provideHttpClient(
       withInterceptors([AuthInterceptor])
-    )
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
