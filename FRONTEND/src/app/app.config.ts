3012fab1959c09a6cf84f6bd8b44a8103118719e
RodrigoSendinoSanz <rodrigosendinosanz@gmail.com>
2025-06-13 16:48:07 +0200

    feat: resolve conflicts

--- FRONTEND/src/app/app.config.ts
+++ FRONTEND/src/app/app.config.ts
@@ -1,22 +1,18 @@
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app.routes';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { isDevMode } from '@angular/core';
 import { provideServiceWorker } from '@angular/service-worker';
+import { SpinnerInterceptor } from './core/interceptors/spinner.interceptor';
+
 
 export const appConfig = {
   providers: [
     provideRouter(routes,withComponentInputBinding()),
     provideHttpClient(
-      withInterceptors([AuthInterceptor])
+      withInterceptors([AuthInterceptor,SpinnerInterceptor])
     ), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
           })
