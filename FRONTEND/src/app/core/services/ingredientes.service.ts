eb916c939cba41904a6f92f9dcfad445ebedca84
Carlos Pérez <carlospe.mateos@gmail.com>
2025-06-15 13:17:26 +0200

    feat: enhance getIngredientes method to support pagination and search functionality

--- FRONTEND/src/app/core/services/ingredientes.service.ts
+++ FRONTEND/src/app/core/services/ingredientes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IIngredientes } from '../../interfaces/iingredientes.interfaces';
 import { lastValueFrom } from 'rxjs';
 @Injectable({
@@ -7,12 +7,30 @@ import { lastValueFrom } from 'rxjs';
 })
 export class IngredientesService {
 
- private httpClient = inject(HttpClient);
+  private httpClient = inject(HttpClient);
   private url: string = 'http://localhost:3000/api/ingredientes';
+  private _totalItems = 0;
 
-  getIngredientes(): Promise<IIngredientes[]> {
-    return lastValueFrom(this.httpClient.get<IIngredientes[]>(this.url));
+  get totalItems() { return this._totalItems; }
+
+  getIngredientes(
+    page: number = 1,
+    limit: number = 10,
+    search: string = ''
+  ): Promise<IIngredientes[]> {
+    const params = new HttpParams()
+      .set('page', page)
+      .set('limit', limit)
+      .set('search', search);
+
+    return lastValueFrom(this.httpClient.get<any>(this.url, { params })).then(
+      (resp) => {
+        this._totalItems = resp.total;
+        return resp.data as IIngredientes[];
+      }
+    );
   }
+
   getIngredienteById(id: number): Promise<IIngredientes> {
     return lastValueFrom(this.httpClient.get<IIngredientes>(`${this.url}/${id}`));
   }
